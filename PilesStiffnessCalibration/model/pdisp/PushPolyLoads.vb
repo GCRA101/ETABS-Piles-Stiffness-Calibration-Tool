497aac68ec6693d48f1d7a467087921211dd27e7
g.albieri <126593217+GCRA101@users.noreply.github.com>
2026-06-07 21:00:29 +0100

    Use Get+SetPolyLoad to force polyload creation in PushPolyLoads.vb

--- PilesStiffnessCalibration/model/pdisp/PushPolyLoads.vb
+++ PilesStiffnessCalibration/model/pdisp/PushPolyLoads.vb
@@ -1,4 +1,6 @@
-﻿Public Class PushPolyLoads
+﻿Imports pdispauto_20_1
+
+Public Class PushPolyLoads
     Inherits PushBehaviour
     Implements PushData
 
@@ -22,18 +24,38 @@
     Public Sub push(overwrite As Boolean) Implements PushData.push
 
         With Me.pDispModel.getPDispApp()
+            ' Variables Declaration
+            Dim numPolyLoads, pli As Short
+            Dim polyLoadLevel As Double
             '1. DELETE EXISTING POLYLOADS IF overwrite=True
             If (overwrite) Then
-                Dim numPolyLoads As Short
                 Do
                     .DeletePolyLoad(1)
                     .NumPolyLoads(numPolyLoads)
                 Loop Until numPolyLoads = 0
             End If
             '2. ADD NEW POLYLOADS
-            Me.polyLoads.ForEach(Sub(polyLd) .AddPolyLoad(polyLd.getLoad()))
+            Try
+                pli = 1
+                For Each polyLd As PDispPolyLoad In Me.polyLoads
+                    Dim pdispLoad As PolyLoad = polyLd.getLoad()
+                    Dim storedLoad As PolyLoad
+                    polyLoadLevel = pdispLoad.Level
+                    .AddPolyLoad(pdispLoad)
+                    .NumPolyLoads(numPolyLoads)
+                    .GetPolyLoad(pli, storedLoad)
+                    .SetPolyLoad(pli, storedLoad)
+                    pli = numPolyLoads + 1
+                Next
+            Catch ex As Exception
+                ' TODO: log or handle the error appropriately
+                System.Diagnostics.Debug.WriteLine($"AddPolyLoad failed: {ex.Message}")
+                Throw ' rethrow to preserve the original stack trace; remove if you want to swallow
+            End Try
         End With
 
+        ' Via Streams
+        ' Me.polyLoads.ForEach(Sub(polyLd) pdispApp.AddPolyLoad(polyLd.getLoad()))
     End Sub
 
 
